bdc6a54122b7c56f8e31593e8d2061d8d9680ab7
Jesús Moreira <jesusromc8@gmail.com>
2020-07-15 13:07:01 -0500

    jjj

--- routes/status.js
+++ routes/status.js
@@ -3,6 +3,10 @@ const router = express.Router();
 
 // Rutas de status
 router.get("/", (req, res, next) => {
+  res.redirect("/status/transfer");
+});
+
+router.get("/transfer", (req, res, next) => {
   res.render("transferir");
 });
 
