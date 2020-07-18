4176f9238e155215b0511765274e60635cfbdf9e
Rackaras <raulcm55@gmail.com>
2020-07-17 21:07:27 -0500

    intentodedeposito

--- routes/status.js
+++ routes/status.js
@@ -88,7 +88,63 @@ router.get("/withdrawal", (req, res, next) => {
 });
 
 router.get("/deposit", (req, res, next) => {
-  res.render("deposit");
+  res.render("deposit" , {user: req.cookies.userData});
+});
+
+router.post("/transfer/check", async (req, res, next) => {
+  try {
+    const db = req.db;
+    const form = req.body;
+    const collection = await db.get("cuentas");
+    const cuentaAcreditada = await collection.find(
+      { numero_cuenta: form.cuentaAcreditar },
+      {}
+    );
+
+    if (cuentaAcreditada.length === 0) {
+      res.render("deposit", {
+        user: req.cookies.userData,
+        error: "La cuenta a acreditar no existe.",
+      });
+    } else if (cuentaAcreditada[0].numero_cuenta === form.cuentasDebitar) {
+      res.render("deposit", {
+        user: req.cookies.userData,
+        error: "No puedes depositarte a ti mismo.",
+      });
+    }
+
+    const cuentaDebitar = await collection.find(
+      { numero_cuenta: form.cuentasDebitar },
+      {}
+    );
+
+    if (cuentaDebitar[0].saldo < form.valor) {
+      res.render("deposit", {
+        user: req.cookies.userData,
+        error: "No tienes el saldo suficiente.",
+      });
+    } else {
+      const valorNuevoDebitar = cuentaDebitar[0].saldo - Number(form.valor);
+      const valorNuevoAcreditar =
+        cuentaAcreditada[0].saldo + Number(form.valor);
+
+      await collection.update(
+        { _id: cuentaAcreditada[0]._id },
+        { $set: { saldo: valorNuevoAcreditar } }
+      );
+      await collection.update(
+        { _id: cuentaDebitar[0]._id },
+        { $set: { saldo: valorNuevoDebitar } }
+      );
+
+      res.render("deposit", {
+        user: req.cookies.userData,
+        exito: "El depósito se realizó correctamente.",
+      });
+    }
+  } catch (err) {
+    console.error(err);
+  }
 });
 
 module.exports = router;
